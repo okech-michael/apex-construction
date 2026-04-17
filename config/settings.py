6685e536559682ac710171d65a7a4c73e133b4d0
okech-michael <okechonyango365@gmail.com>
2026-04-17 23:04:55 +0300

    Temporarily set DEBUG=True for deployment debugging

--- config/settings.py
+++ config/settings.py
@@ -9,7 +9,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 SECRET_KEY = os.environ.get('SECRET_KEY', 'django-insecure-apex-construction-change-this-in-production-xyz123')
 
-DEBUG = os.environ.get('DEBUG', 'False').lower() == 'true'
+DEBUG = True
 
 ALLOWED_HOSTS = os.environ.get('ALLOWED_HOSTS', '*').split(',') if os.environ.get('ALLOWED_HOSTS') else ['*']
 
